237e4c6e1e987a382e826ed9de0851c42920e1b9
ademar.gonzalez <ademar.gonzalez@21d1304e-a24e-0410-9a17-a713df6cd0cc>
2008-12-09 21:01:54 +0000

    git-svn-id: https://melon-reports.googlecode.com/svn/trunk@10 21d1304e-a24e-0410-9a17-a713df6cd0cc

---
+++ Melon.Commons/RgbColor.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Globalization;
+
+namespace Melon.Commons
+{
+	/// <summary>
+	/// Summary description for Color.
+	/// </summary>
+	public class RgbColor
+	{
+		public RgbColor()
+		{
+			Blue = "0.0";
+			Green = "0.0";
+			Red = "0.0";
+		}
+
+		public RgbColor(System.Drawing.Color color)
+		{
+			Red = FixDouble((double)color.R / 255); //TODO : check pdf literals definition
+			Green = FixDouble((double)color.G / 255);
+			Blue = FixDouble((double)color.B / 255);
+		}
+
+		public RgbColor(string color)
+			: this(System.Drawing.Color.FromArgb(Convert.ToInt32(color.Substring(1), 16)))
+		{
+			
+		}
+
+		private static string FixDouble(double dbl)
+		{
+			var formater = new NumberFormatInfo {NumberDecimalSeparator = "."};
+
+			var str = dbl.ToString(formater);
+			int dec = str.IndexOf(".");
+
+			if (dec != -1)
+			{
+				if ((str.Length - dec) > 6) str = str.Substring(0, dec + 6);
+				return str;
+			}
+			return str;
+		}
+
+		public string Red { get; private set; }
+
+		public string Green { get; private set; }
+
+		public string Blue { get; private set; }
+	}
+}
